1591607e152b182d693d8f622e4761f191d85e0d
ikiK <kiki.ikik@gmail.com>
2021-06-06 16:48:58 +0200

    -a

--- DocumentRoot/api/api.php
+++ DocumentRoot/api/api.php
@@ -3,60 +3,100 @@ header("Content-Type:application/json");
 header("Access-Control-Allow-Origin: *");
 require('db.php');
 
-// GET SVE NEKRETNINE
-if (isset($_GET['nekretnine'])) {
-    $sql = mysqli_query($con, "SELECT * FROM `nekretnine`");
-    $data = array();
-    while ($row = $sql->fetch_assoc()) {
-        $data[] = $row;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // GET SVE NEKRETNINE
+    if (isset($_GET['nekretnine'])) {
+        $sql = mysqli_query($con, "SELECT * FROM `nekretnine`");
+        $data = array();
+        while ($row = $sql->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
     }
-    echo json_encode($data);
-}
 
-// GET NEKRETNINU
-if (isset($_GET['nekretnina']) && $_GET['nekretnina'] != "") {
-    $id = mysqli_real_escape_string($con, $_GET['nekretnina']);
+    // GET NEKRETNINU
+    if (isset($_GET['nekretnina']) && $_GET['nekretnina'] != "") {
+        $id = mysqli_real_escape_string($con, $_GET['nekretnina']);
 
-    $sql = mysqli_query($con, "SELECT * FROM `nekretnine` WHERE nek_id = '$id'");
-    $data = array();
-    while ($row = $sql->fetch_assoc()) {
-        $data[] = $row;
+        $sql = mysqli_query($con, "SELECT * FROM `nekretnine` WHERE nek_id = '$id'");
+        $data = array();
+        while ($row = $sql->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
     }
-    echo json_encode($data);
-}
 
+    //LOGIN
+    if (isset($_GET['korisnik']) && $_GET['korisnik'] != "") {
+        $korisnik = mysqli_real_escape_string($con, $_GET['korisnik']);
+        $lozinka = mysqli_real_escape_string($con, $_GET['lozinka']);
 
-if (isset($_GET['korisnik']) && $_GET['korisnik'] != "") {
-    $korisnik = mysqli_real_escape_string($con, $_GET['korisnik']);
-    $lozinka = mysqli_real_escape_string($con, $_GET['lozinka']);
-
-    $sql = mysqli_query($con, "SELECT * FROM `users` WHERE usr_name = '$korisnik' AND usr_pass = '$lozinka'");
-    $data = array();
-    while ($row = $sql->fetch_assoc()) {
-        $data[] = $row;
-    }
-    echo json_encode($data);
-}
-
-// if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-//     echo "true";
-// }
-
-if (isset($_GET['unos']) && $_GET['unos'] != "") {
-    $naslov = mysqli_real_escape_string($con, $_GET['unos']);
-    $cijena = mysqli_real_escape_string($con, $_GET['cijena']);
-    $kat = mysqli_real_escape_string($con, $_GET['kat']);
-
-    $sql = "INSERT INTO nekretnine (nek_naslov, nek_cijena, kat_id) VALUES ('$naslov', '$cijena', '$kat')";
-
-    if ($con->query($sql) === TRUE) {
-        echo "true";
-    } else {
-        echo "Error: " . $sql . "<br>" . $con->error;
+        $sql = mysqli_query($con, "SELECT * FROM `users` WHERE usr_name = '$korisnik' AND usr_pass = '$lozinka'");
+        $data = array();
+        while ($row = $sql->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
     }
 }
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
+    if (isset($_GET['edit']) && $_GET['edit'] != "") {
+        $naslov = mysqli_real_escape_string($con, $_GET['edit']);
+        $cijena = mysqli_real_escape_string($con, $_GET['cijena']);
+        $kat = mysqli_real_escape_string($con, $_GET['kat']);
+        $id = mysqli_real_escape_string($con, $_GET['id']);
+
+        $sql = "UPDATE nekretnine SET nek_naslov='$naslov', nek_cijena='$cijena', kat_id='$kat' WHERE nek_id='$id'";
+
+        if ($con->query($sql) === TRUE) {
+            echo "true";
+        } else {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
+    }
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_GET['unos']) && $_GET['unos'] != "") {
+        $naslov = mysqli_real_escape_string($con, $_GET['unos']);
+        $cijena = mysqli_real_escape_string($con, $_GET['cijena']);
+        $kat = mysqli_real_escape_string($con, $_GET['kat']);
+
+        $sql = "INSERT INTO nekretnine (nek_naslov, nek_cijena, kat_id) VALUES ('$naslov', '$cijena', '$kat')";
+
+        if ($con->query($sql) === TRUE) {
+            echo "true";
+        } else {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    // DELETE
+    if (isset($_GET['delete']) && $_GET['delete'] != "") {
+        $id = mysqli_real_escape_string($con, $_GET['delete']);
+
+        $sql = "DELETE FROM nekretnine WHERE nek_id='$id'";
+
+        if ($con->query($sql) === TRUE) {
+            echo "true";
+        } else {
+            echo "Error deleting record: " . $con->error;
+        }
+    }
+}
+
 
 if (isset($_FILES['file']['name'])) {
+    $id = $_POST['id'];
     $filename = $_FILES['file']['name'];
 
     $location = '../src/img/' . $filename;
@@ -69,7 +109,12 @@ if (isset($_FILES['file']['name'])) {
     if (in_array($file_extension, $valid_ext)) {
         // Upload file
         if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
-            $sql = "UPDATE nekretnine SET nek_img='$location' ORDER BY nek_id DESC LIMIT 1";
+            if (!empty($id)) {
+                $sql = "UPDATE nekretnine SET nek_img='$location' WHERE nek_id = '$id'";
+            } else {
+                $sql = "UPDATE nekretnine SET nek_img='$location' ORDER BY nek_id DESC LIMIT 1";
+            }
+
 
             if ($con->query($sql) === TRUE) {
                 $response = 1;
@@ -81,18 +126,4 @@ if (isset($_FILES['file']['name'])) {
 
     echo $response;
     exit;
-}
-
-
-// DELETE
-if (isset($_GET['delete']) && $_GET['delete'] != "") {
-    $id = mysqli_real_escape_string($con, $_GET['delete']);
-
-    $sql = "DELETE FROM nekretnine WHERE nek_id='$id'";
-
-    if ($con->query($sql) === TRUE) {
-        echo "true";
-    } else {
-        echo "Error deleting record: " . $con->error;
-    }
 }
--- DocumentRoot/index.js
+++ DocumentRoot/index.js
@@ -33,12 +33,26 @@ var toastList = toastElList.map(function (toastEl) {
 
 const toastText = document.querySelector('.toast-body')
 
-// const fetchOptDELETE = {
-//   method: 'DELETE',
-//   headers: {
-//       'Content-Type': 'application/json'
-//   }
-// }
+const fetchOptDELETE = {
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+const fetchOptPOST = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+const fetchOptPUT = {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
 
 const vm = createApp({
   data () {
@@ -53,7 +67,7 @@ const vm = createApp({
   },
   methods: {
     del: function (id) {
-      fetch(api + '?delete=' + id)
+      fetch(api + '?delete=' + id, fetchOptDELETE)
         .then(response => {
           if (response.ok) {
             return response.json()
@@ -76,9 +90,11 @@ const vm = createApp({
       let item = this.$data.obj.filter(e => e.nek_id == id)
       naziv.value = item[0].nek_naslov
       cijena.value = item[0].nek_cijena
-      kat.selectedIndex = item[0].kat_id 
+      kat.selectedIndex = item[0].kat_id
       // document.querySelector('#file').value = item[0].nek_img
       // console.log(item[0].nek_img)
+      $('#exampleModal2 .modal-title').attr('data-nekid', id)
+      $('#exampleModal2 .modal-title').text('EDITIRAJ NEKRETNINU')
       $('#exampleModal2').modal('show')
     },
     refresh (data) {
@@ -146,47 +162,102 @@ document.querySelector('#add').addEventListener('click', e => {
   cijena.value = ''
   fileEl.value = ''
   kat.selectedIndex = 0
+  $('#exampleModal2 .modal-title').text('DODAJ NEKRETNINU')
 })
 
 document.querySelector('#unos').addEventListener('click', e => {
-  if (naziv.value != '' && cijena.value != '' && kat != '') {
-    fetch(
-      api +
-        '?unos=' +
-        naziv.value +
-        '&cijena=' +
-        cijena.value +
-        '&kat=' +
-        kat.value
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        } else {
-          throw new Error('NETWORK RESPONSE ERROR')
-        }
-      })
-      .then(data => {
-        //console.log(data)
-        if (data == true) {
-          if (fileEl.value != '') {
-            uploadFile(fileEl)
+  if ($('#exampleModal2 .modal-title').text() === 'EDITIRAJ NEKRETNINU') {
+    const nekid = $('#exampleModal2 .modal-title').attr('data-nekid')
+
+    if (naziv.value != '' && cijena.value != '' && kat != '') {
+      fetch(
+        api +
+          '?edit=' +
+          naziv.value +
+          '&cijena=' +
+          cijena.value +
+          '&kat=' +
+          kat.value +
+          '&id=' +
+          nekid,
+        fetchOptPUT
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json()
           } else {
-            $('#exampleModal2').modal('hide')
-            toastText.innerHTML = 'Unos uspješan!'
-            toastList.forEach(toast => toast.show())
-            newAppData()
+            throw new Error('NETWORK RESPONSE ERROR')
           }
-        }
-      })
+        })
+        .then(data => {
+          console.log(data)
+          if (data == true) {
+            if (fileEl.value != '') {
+              uploadFile(fileEl, nekid)
+            } else {
+              $('#exampleModal2').modal('hide')
+              toastText.innerHTML = 'Unos uspješan!'
+              toastList.forEach(toast => toast.show())
+              newAppData()
+              setTimeout(function () {
+                $('.bi-pencil-square').fadeIn()
+                $('.bi-trash-fill').fadeIn()
+              }, 1000)
+            }
+          }
+        })
+    }
+  } else {
+    if (naziv.value != '' && cijena.value != '' && kat != '') {
+      fetch(
+        api +
+          '?unos=' +
+          naziv.value +
+          '&cijena=' +
+          cijena.value +
+          '&kat=' +
+          kat.value,
+        fetchOptPOST
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('NETWORK RESPONSE ERROR')
+          }
+        })
+        .then(data => {
+          //console.log(data)
+          if (data == true) {
+            if (fileEl.value != '') {
+              uploadFile(fileEl)
+            } else {
+              $('#exampleModal2').modal('hide')
+              toastText.innerHTML = 'Unos uspješan!'
+              toastList.forEach(toast => toast.show())
+              newAppData()
+              setTimeout(function () {
+                $('.bi-pencil-square').fadeIn()
+                $('.bi-trash-fill').fadeIn()
+              }, 1000)
+            }
+          }
+        })
+    }
   }
 })
 
 // Upload file
-function uploadFile (fileEl) {
+function uploadFile (fileEl, id) {
+  console.log(id)
   var files = fileEl.files
   var formData = new FormData()
   formData.append('file', files[0])
+
+  if (id != null) {
+    formData.append('id', id)
+  }
+
   var xhttp = new XMLHttpRequest()
   xhttp.open('POST', api, true)
 
